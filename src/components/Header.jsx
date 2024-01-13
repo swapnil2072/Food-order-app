32131dbbd820fc506092bfd0c6435c0fa5748659
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-13 22:31:48 +0530

    opening cart in the modal via new context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,21 @@ import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Header = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
-  const totalNumberOfItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
-    return totalNumberOfItems + item.quantity;
-  }, 0);
+  const totalNumberOfItems = cartCtx.items.reduce(
+    (totalNumberOfItems, item) => {
+      return totalNumberOfItems + item.quantity;
+    },
+    0,
+  );
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -16,7 +24,9 @@ const Header = () => {
         <h1>Food app</h1>
       </div>
       <nav>
-        <Button textOnly>Cart({totalNumberOfItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart({totalNumberOfItems})
+        </Button>
       </nav>
     </header>
   );
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { createPortal } from "react-dom";
+import { useRef } from "react";
 
 const Modal = ({ children, open, className = "" }) => {
   const dialog = useRef();
   useEffect(() => {
+    const modal = dialog.current;
     if (open) {
-      dialog.current.showModal();
+      modal.showModal();
     }
+    return () => modal.close();
   }, [open]);
   return createPortal(
     <dialog ref={dialog} className={`modal ${className}`}>
