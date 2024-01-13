feb617e1b707cc93d0f09d83bab56f6801a91dd4
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-13 22:43:48 +0530

    worked on cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { currencyFormatter } from "../util/formatting";
 
-const CartItem = () => {
+const CartItem = ({ name, quantity, price, onIncrease, onDecrease }) => {
   return (
-    <div>CartItem</div>
-  )
-}
+    <li className="cart-item">
+      <p>
+        {name} - {quantity} x {currencyFormatter.format(price)}
+      </p>
+      <p className="cart-item-actions">
+        <button onClick={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </p>
+    </li>
+  );
+};
 
-export default CartItem
+export default CartItem;
