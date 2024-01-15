e800f6cce5e6e139bd6d88a05af89841dc00813d
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-15 21:06:15 +0530

    adding a custom http hook

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Modal from "./UI/Modal";
 import { useContext } from "react";
-import CartContext from "../store/CartContext";
-import { currencyFormatter } from "../util/formatting";
-import Input from "./UI/Input";
-import UserProgressContext from "../store/UserProgressContext";
-import Button from "./UI/Button";
 
-const Checkout = () => {
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext.jsx";
+import { currencyFormatter } from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
@@ -20,34 +20,47 @@ const Checkout = () => {
     userProgressCtx.hideCheckout();
   }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
 
-     const fd = new FormData(event.target);
-     const customerData = Object.fromEntries(fd.entries());
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
-        <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
-        <Input label="Full Name" type="text" id="full-name" />
-        <Input label="E-mail Address" type="email" id="email" />
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
+
         <p className="modal-actions">
-          <Button textOnly type="button" onClick={handleClose}>
+          <Button type="button" textOnly onClick={handleClose}>
             Close
           </Button>
-          <Button textOnly>Submit Order</Button>
+          <Button>Submit Order</Button>
         </p>
       </form>
     </Modal>
   );
-};
-
-export default Checkout;
+}
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from "react";
-import MealItem from "./MealItem";
+import MealItem from "./MealItem.jsx";
+import useHttp from "./hooks/useHttp.js";
 
-const Meals = () => {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+const requestConfig = {};
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-        //..
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
+
+  // if (!data) {
+  //   return <p>No meals found.</p>
+  // }
 
   return (
     <ul id="meals">
@@ -23,6 +25,4 @@ const Meals = () => {
       ))}
     </ul>
   );
-};
-
-export default Meals;
+}
